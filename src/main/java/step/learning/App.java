c304a945ba5be8293f46249e2c156b1b640effc3
Murderzy <anntarasiuk@gmail.com>
2022-10-16 18:27:59 +0300

    add a lot of text

--- src/main/java/step/learning/App.java
+++ src/main/java/step/learning/App.java
@@ -2,14 +2,11 @@ package step.learning;
 
 import com.mysql.cj.jdbc.Driver;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Random;
 
 public class App {
-    public void run()
-    {
+    public void run() {
         //регистрируем драйвер для бд
         Driver mysqlDriver;
         System.out.println("App works");
@@ -29,10 +26,10 @@ public class App {
 
         //подключение
         String connectionString =
-                "jdbc:mysql://localhost:3306/java191?useUnicode=true&characterEncoding=UTF-8" ;
+                "jdbc:mysql://localhost:3306/java191?useUnicode=true&characterEncoding=UTF-8";
         Connection connection;
         try {
-            connection = DriverManager.getConnection(connectionString,"user191","pass191");
+            connection = DriverManager.getConnection(connectionString, "user191", "pass191");
         } catch (SQLException e) {
             System.out.println("Connection Error");
             return;
@@ -45,24 +42,100 @@ public class App {
                 "str VARCHAR(64) NULL" +
                 ") Engine=InnoDB DEFAULT CHARSET = UTF8";
 
-        try
-        {
+        try {
             Statement statement = connection.createStatement();
             statement.executeUpdate(sql);  //  вариант без возврата данных
             System.out.println("Query OK");
-        }catch (SQLException e) {
-            System.out.println("Error");
-            return;
-        }
-
-        try {
-            connection.close();  //  закрываем подключение
-            DriverManager.deregisterDriver(mysqlDriver);
         } catch (SQLException e) {
             System.out.println("Error");
             return;
         }
+        Random rnd = new Random();
+        int rndInt = rnd.nextInt();
+        String rndStr = "Str" + rndInt;
 
+        sql = String.format("INSERT INTO randoms VALUES (UUID_SHORT(),%d, '%s')", rndInt, rndStr);
+
+        try (
+                Statement statement = connection.createStatement();
+        ) {
+            statement.executeUpdate(sql);
+            System.out.println("Insert OK");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println(sql);
+            return;
+        }
+
+        sql = "SELECT * FROM randoms";
+        try (Statement statement = connection.createStatement()) {
+            ResultSet res = statement.executeQuery(sql);
+
+            while (res.next()) {
+                System.out.printf("%d %d %s %n", res.getLong(1),
+                        res.getInt(2), res.getString("str"));
+            }
+            ;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println(sql);
+            return;
+        }
+
+        sql = "SELECT r.id, r.num, r.str FROM randoms AS r";
+        try (Statement statement = connection.createStatement()) {
+            ResultSet res = statement.executeQuery(sql);
+
+            while (res.next()) {
+                System.out.printf("%d %d %s %n",
+                        res.getLong(1), res.getInt("num"), res.getString(3));
+            }
+            ;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println(sql);
+            return;
+        }
+
+        //
+
+        sql = "INSERT INTO randoms VALUES (UUID_SHORT(),?,?)";
+
+        try (PreparedStatement prep = connection.prepareStatement(sql)) {
+            for (int i = 100500; i < 100503; ++i) {
+
+                rndStr = "Prep " + rnd.nextInt();
+
+                prep.setInt(1, i);
+                prep.setString(2, rndStr);
+                prep.executeUpdate();
+            }
+            System.out.println("prep ok");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            System.out.println(sql);
+            return;
+        }
+        System.out.println("------------------------------------------------");
+        sql = "SELECT * FROM randoms ORDER BY num DESC ";
+        try (Statement statement = connection.createStatement();
+             ResultSet res = statement.executeQuery(sql)) {
+            while (res.next()) {
+                System.out.printf("%d  %d  %s %n",
+                        res.getLong(1), res.getInt("num"), res.getString(3));
+            }
+            try {
+                connection.close();  //  закрываем подключение
+                DriverManager.deregisterDriver(mysqlDriver);
+            } catch (SQLException e) {
+                System.out.println("Error");
+                return;
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
